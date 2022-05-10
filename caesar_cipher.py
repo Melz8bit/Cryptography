f10d8ac92f3031afd3906cbdff6a60976413d500
Melz8bit <37233103+Melz8bit@users.noreply.github.com>
2022-05-09 20:44:27 -0400

    Caesar Cipher

---
+++ caesar_cipher.py
@@ -0,0 +1,34 @@
+from hashlib import new
+from re import A
+import string
+
+CHARACTERS = string.ascii_letters + string.digits + string.punctuation
+
+user_input = input("Enter text: ")
+method = ''
+
+while method != 'e' and method != 'd':
+    method = input("Is this an (E)ncryption or (D)ecryption? ").lower()
+
+key = int(input("What is the key? "))
+
+translated = ''
+for character in user_input:
+    index = CHARACTERS.index(character)
+    
+    new_index = 0
+    if method == 'e':
+        new_index = index + key
+
+        if new_index >= len(CHARACTERS):
+            new_index -= len(CHARACTERS)
+    
+    else:
+        new_index = index - key
+
+        if new_index < 0:
+            new_index += len(CHARACTERS)
+        
+    translated += CHARACTERS[new_index]
+
+print(translated)
