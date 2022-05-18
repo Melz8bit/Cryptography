b08cde19af6ec0b147f9a0eeadd5b785a1d3f414
Melz8bit <37233103+Melz8bit@users.noreply.github.com>
2022-05-17 21:08:47 -0400

    Added decryption

--- simple_transposition.py
+++ simple_transposition.py
@@ -1,6 +1,23 @@
+import math
 
-from pydoc import plain
+def decrypt_message(ciphertext: str, key):
+    dec_key = math.ceil(len(ciphertext) / key)
+    plaintext = [''] * dec_key
 
+    shaded_boxes = (key * dec_key) - len(ciphertext)
+
+    column = 0
+    row = 0
+
+    for character in ciphertext:
+        plaintext[column] += character
+        column += 1
+
+        if column == dec_key or (column == dec_key - 1 and row >= key - shaded_boxes):
+            column = 0
+            row += 1
+    
+    return ''.join(plaintext)
 
 def encrypt_message(plaintext, key):
     ciphertext = [''] * key
@@ -12,14 +29,18 @@ def encrypt_message(plaintext, key):
 
             index += key
 
-    return ''.join(ciphertext)
-    
+    return ''.join(ciphertext)    
 
 def main():
-    plaintext = input("Enter plaintext: ")
+    method = input("(E)ncryption or (D)ecryption? ")
+    text = input("Enter message: ")
     key = int(input("Enter key length: "))
 
-    print(encrypt_message(plaintext, key))
+    if method.lower() == 'e':
+        print(f'{encrypt_message(text, key)}|')
+    elif method.lower() == 'd':
+        print(decrypt_message(text, key))
+
 
 if __name__ == '__main__':
     main()
